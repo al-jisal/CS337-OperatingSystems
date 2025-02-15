1e2d1c7162a0388cf0b8d7637459eae4e50b6876
al_jisal <aljisal.frimpong@gmail.com>
2025-02-15 11:09:30 -0500

    created FCFS schejuling algorithm

---
+++ Project01/scheduler.py
@@ -0,0 +1,44 @@
+"""
+The scheduler contains programs for scheduling processes on the CPU
+"""
+def FCFS_scheduler(processes, # list of all the processes in the simulation, whether arrived or not
+                   ready, # list of processes with current arrival time
+                   CPU, # list that holds beginnig and end runtimes for each process
+                   time, # an integer representing the current time
+                   verbose=True):
+    """non-preemptive FCFS scheduler"""
+    process = find_lowest_arrival(ready)
+    start_time = time
+    
+    while process.burst_time > 0:
+        process.burst_time -= 1
+        time += 1
+        add_ready(processes, ready, time)
+
+    end_time = time
+    CPU.append( dict(process=process.get_ID(),
+                     Start=start_time,
+                     Finish=end_time,
+                     Priority=process.get_priority()))
+    return time
+
+def find_lowest_arrival(ready):
+    """returns the process with the lowest arrival time in the ready queue"""
+    if len(ready) == 0:
+        return None
+    
+    process = None
+    process_arrival_time = float("inf")
+    for item in ready:
+        if item.arrival_time < process_arrival_time:
+            process = item
+            process_arrival_time =item.arrival_time
+    ready.remove(process)
+    return process
+
+
+def add_ready(processes, ready, time):
+    """adds processes arriving at the current time to the ready queue"""
+    for process in processes:
+        if process.get_arrival_time() == time:
+            ready.append(process)
