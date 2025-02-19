6e70aa2163df08bf3b04da1550d0f78a6b450d0a
al_jisal <aljisal.frimpong@gmail.com>
2025-02-18 19:00:37 -0500

    removed the hardcoded processes from kernel(), allowing it to accepts processes as a parameter

--- Project01/operating_system.py
+++ Project01/operating_system.py
@@ -5,22 +5,11 @@ In this file,
     - statistics are calculated
 """
 import pandas as pd
-import process, scheduler
+import scheduler
 
-def kernel(selected_scheduler, verbose=True):
+def kernel(selected_scheduler, processes, verbose=True):
     """Simulates CPU scheduling aspect of an operating system kernel"""
-    CPU, ready, processes, time = [], [], [], 0
-
-    # creating processes
-    process0 = process.Process(0, 5, 0, 30)
-    process1 = process.Process(1, 4, 2, 35)
-    process2 = process.Process(2, 1, 5, 36)
-    process3 = process.Process(3, 6, 6, 20)
-
-    processes.append(process0)
-    processes.append(process1)
-    processes.append(process2)
-    processes.append(process3)
+    CPU, ready, time = [],[], 0
 
     scheduler.add_ready(processes, ready, time)
 
