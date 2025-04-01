926f70bee8e01cc7dea39e6e4877c5908ff8ca00
al_jisal <aljisal.frimpong@gmail.com>
2025-04-01 13:01:46 -0400

    added a file for sequential programming

---
+++ Project05/serial_code.py
@@ -0,0 +1,9 @@
+"""
+    This is  a program to process eight years of Reddit comments to find the total word count
+    and the frequency of each word in each year and collectively. The program implements a base
+    version of the code that does not use any multitasking concepts to speed up the code. This 
+    program is used as a base-line for comparison.
+"""
+
+def main():
+    pass
