2de5f3c6367fca199fbc32ce46acb3f9c9a64bab
al_jisal <aljisal.frimpong@gmail.com>
2025-04-01 13:13:46 -0400

    added a file that will containg the multitasking program

---
+++ Project05/multitasking_code.py
@@ -0,0 +1,8 @@
+"""
+    This is  a program to process eight years of Reddit comments to find the total word count
+    and the frequency of each word in each year and collectively. This program uses creative 
+    multitasking skills to speed up the code
+"""
+
+def main():
+    pass
