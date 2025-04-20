d652df426d063f74d54c67d40b4069b379af5e94
al_jisal <aljisal.frimpong@gmail.com>
2025-04-19 22:03:33 -0400

    deleted project 5

--- Project05/multitasking_code.py
+++
@@ -1,8 +0,0 @@
-"""
-    This is  a program to process eight years of Reddit comments to find the total word count
-    and the frequency of each word in each year and collectively. This program uses creative 
-    multitasking skills to speed up the code
-"""
-
-def main():
-    pass
